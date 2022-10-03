93efb240b9d6b3b3eb60dd31dd603f5981e76ee7
gs.lee <gs.lee@cndfactory.com>
2022-10-04 00:56:20 +0900

    unused file remove

--- src/components/demo/index.js
+++
@@ -1,6 +0,0 @@
-import DemoPanel from './DemoPanel.svelte';
-import Play from './Play.svelte';
-import Description from './Description.svelte';
-import Properties from './Properties.svelte';
-
-export { DemoPanel, Play, Description, Properties };
